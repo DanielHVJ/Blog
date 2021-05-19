3560e52a0bccc53040514332e1cf28218fc4eda9
DanyP <danielvediaj@gmail.com>
2021-05-19 20:35:53 +0200

    mid 3

--- Statistics/coinf.py
+++ Statistics/coinf.py
@@ -65,7 +65,7 @@ import math
 
 n = 20
 k = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-k1 = int(np.linspace(0,1,21))
+k1 = (np.linspace(0,1,21))
 distr = [0]*21
 out_arc = [0]*21
 
@@ -80,23 +80,25 @@ for i in k:
     # out_arc[i] = np.sin(i)
     i+=1
 
-vec = np.vectorize(np.int64)
-k2 = vec(k1)
-
-for i in (k2):    
-    out_arc[i] = np.arcsin(i)
-    i+=1
-
 out_arc[0] = np.arcsin(k1[0])
+out_arc[1] = np.arcsin(k1[1])
+out_arc[2] = np.arcsin(k1[2])
 out_arc[3] = np.arcsin(k1[3])
+out_arc[4] = np.arcsin(k1[4])
 out_arc[5] = np.arcsin(k1[5])
+out_arc[6] = np.arcsin(k1[6])
 out_arc[7] = np.arcsin(k1[7])
+out_arc[8] = np.arcsin(k1[8])
 out_arc[9] = np.arcsin(k1[9])
 out_arc[10] = np.arcsin(k1[10])
+out_arc[11] = np.arcsin(k1[11])
+out_arc[12] = np.arcsin(k1[12])
 out_arc[13] = np.arcsin(k1[13])
-out_arc[13] = np.arcsin(k1[13])
+out_arc[14] = np.arcsin(k1[14])
 out_arc[15] = np.arcsin(k1[15])
+out_arc[16] = np.arcsin(k1[16])
 out_arc[17] = np.arcsin(k1[17])
+out_arc[18] = np.arcsin(k1[18])
 out_arc[19] = np.arcsin(k1[19])
 out_arc[20] = np.arcsin(k1[20])
 
@@ -104,8 +106,8 @@ import matplotlib.pyplot as plt
 
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax2.bar(k1, out_arc, color = 'pink', alpha = 0.3,edgecolor='None')
-ax1.bar(k1, distr,color = 'b', alpha = 0.8,edgecolor='None')
+ax2.plot(k1, out_arc, color = 'pink', alpha = 0.5)
+ax1.plot(k1, distr,color = 'b', alpha = 0.8)
 plt.show()
 
 np.arcsin(0.05)
